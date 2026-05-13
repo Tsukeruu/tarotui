7eba0a200aa63f43b254cbd30d7dddef7b15c158
Tsukeru <uzdbald@gmail.com>
2026-05-13 12:37:18 +0300

    fixed inputs

--- src/utils/custom_errors.py
+++ src/utils/custom_errors.py
@@ -2,7 +2,7 @@ from typing import Any
 from requests.exceptions import RequestException
 
 class API_ERROR(Exception):
-    def __init__(self, message: Union[str, RequestException] = None, error: Any =requests.exceptions.RequestException) -> None:
+    def __init__(self, message: Union[str, RequestException] = None, error: RequestException = None) -> None:
         if not message:
             message = f"AN UNKNOWN REQUEST ERROR HAS OCCURED! {error}"
         super().__init__(message)
