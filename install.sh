e63b9ccc8a6990937c5abd8447ffe3a20b88bdca
Tsukeru <uzdbald@gmail.com>
2026-05-13 12:21:32 +0300

    fixed inputs

---
+++ install.sh
@@ -0,0 +1,20 @@
+#An install script for tarotui
+
+# ▀▄    ▀█▀ █▀█ █▀▄ █▀█ ▀█▀ █ █ ▀█▀
+#  ▄▀    █  █▀█ █▀▄ █ █  █  █ █  █ 
+# ▀      ▀  ▀ ▀ ▀ ▀ ▀▀▀  ▀  ▀▀▀ ▀▀▀
+
+#Usually my install scripts tend to be longer, however since this is a pure python project with no mixed languages, it tends to just be an installation
+#Of the libraries
+
+main() {
+  pip install -r libraries.txt
+  read -p "Would you like to run tarotui? (Y/N) " prompt
+  if [[ "$prompt" = "Y" || "$prompt" = "y" ]]; then
+    python tarot.py
+  else
+    exit
+  fi
+}
+
+main
