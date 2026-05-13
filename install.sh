2c4b11e944c409caa19b0f4d7b72e43b0a1710fc
Tsukeru <uzdbald@gmail.com>
2026-05-13 13:42:55 +0300

    fixed inputs

--- install.sh
+++ install.sh
@@ -6,13 +6,19 @@
 #  ▄▀    █  █▀█ █▀▄ █ █  █  █ █  █ 
 # ▀      ▀  ▀ ▀ ▀ ▀ ▀▀▀  ▀  ▀▀▀ ▀▀▀
 
-#Usually my install scripts tend to be longer, however since this is a pure python project with no mixed languages, it tends to just be an installation
-#Of the libraries
+#Usually my install scripts tend to be longer, however since this is a pure python project with no mixed languages, and just one external dependency (ollama), it tends to just be an installation
+#Of the libraries and the building of an ollama model
 
 main() {
   pip install -r libraries.txt
-  read -p "Would you like to run tarotui? (Y/N) " prompt
-  if [[ "$prompt" = "Y" || "$prompt" = "y" ]]; then
+  read -p "Would you like to build the ollama model required for the reading? (Y/N)" ollamaPrompt
+  if [[ "$ollamaPrompt" = "Y" || "$ollamaPrompt" = "y" ]]; then
+    ollama create tarotui -f src/utils/Ollama_custom/Modelfile
+  else
+    continue
+  fi
+  read -p "Would you like to run tarotui? (Y/N) " runPrompt
+  if [[ "$runPrompt" = "Y" || "$runPrompt" = "y" ]]; then
     python tarot.py
   else
     exit
