59982e86ee0480aa5f3b483513b27dc20b9c11df
Tsukeru <uzdbald@gmail.com>
2026-05-14 11:47:02 +0300

    installation script

--- install.sh
+++ install.sh
@@ -9,10 +9,24 @@
 #Usually my install scripts tend to be longer, however since this is a pure python project with no mixed languages, and just one external dependency (ollama), it tends to just be an installation
 #Of the libraries and the building of an ollama model
 
+echo '''
+▀▄    ▀█▀ █▀█ █▀▄ █▀█ ▀█▀ █ █ ▀█▀
+ ▄▀    █  █▀█ █▀▄ █ █  █  █ █  █ 
+▀      ▀  ▀ ▀ ▀ ▀ ▀▀▀  ▀  ▀▀▀ ▀▀▀
+- The installation uses pip install to download the libraries
+- The installation installs ollama 
+''' 
+
 main() {
+  echo "Installing the required python libraries"
   pip install -r libraries.txt
-  read -p "Would you like to build the ollama model required for the reading? (Y/N)" ollamaPrompt
+  if ! command -v "ollama" > /dev/null; then
+    read -p "Would you like to install ollama and build the ollama model required for the reading? (Y/N) " ollamaPrompt
+  else
+    echo "OLLAMA ALREADY INSTALLED, PROCEEDING TO RUN TAROTUI..."
+  fi
   if [[ "$ollamaPrompt" = "Y" || "$ollamaPrompt" = "y" ]]; then
+    curl -fsSL https://ollama.com/install.sh | sh
     ollama create tarotui -f src/utils/Ollama_custom/Modelfile
   else
     continue
