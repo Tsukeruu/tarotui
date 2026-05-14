fe0c553dd25d9e8af2f2d7f2d5b2311b5163aa0e
Tsukeru <uzdbald@gmail.com>
2026-05-14 12:09:26 +0300

    installation script

--- install.sh
+++ install.sh
@@ -9,12 +9,14 @@
 #Usually my install scripts tend to be longer, however since this is a pure python project with no mixed languages, and just one external dependency (ollama), it tends to just be an installation
 #Of the libraries and the building of an ollama model
 
+clear
+
 echo '''
 ▀▄    ▀█▀ █▀█ █▀▄ █▀█ ▀█▀ █ █ ▀█▀
  ▄▀    █  █▀█ █▀▄ █ █  █  █ █  █ 
 ▀      ▀  ▀ ▀ ▀ ▀ ▀▀▀  ▀  ▀▀▀ ▀▀▀
-- The installation uses pip install to download the libraries
-- The installation installs ollama 
+- The installation uses pip to install the required python libraries
+- The installation installs ollama alongside building the custom model 
 ''' 
 
 main() {
@@ -22,14 +24,14 @@ main() {
   pip install -r libraries.txt
   if ! command -v "ollama" > /dev/null; then
     read -p "Would you like to install ollama and build the ollama model required for the reading? (Y/N) " ollamaPrompt
+    if [[ "$ollamaPrompt" = "Y" || "$ollamaPrompt" = "y" ]]; then
+      curl -fsSL https://ollama.com/install.sh | sh
+      ollama create tarotui -f src/utils/Ollama_custom/Modelfile
+    else
+      continue
+    fi
   else
     echo "OLLAMA ALREADY INSTALLED, PROCEEDING TO RUN TAROTUI..."
-  fi
-  if [[ "$ollamaPrompt" = "Y" || "$ollamaPrompt" = "y" ]]; then
-    curl -fsSL https://ollama.com/install.sh | sh
-    ollama create tarotui -f src/utils/Ollama_custom/Modelfile
-  else
-    continue
   fi
   read -p "Would you like to run tarotui? (Y/N) " runPrompt
   if [[ "$runPrompt" = "Y" || "$runPrompt" = "y" ]]; then
