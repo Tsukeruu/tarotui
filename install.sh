c57addaca8767d10e279c5f232d17c34063fb802
Tsukeru <uzdbald@gmail.com>
2026-05-14 15:23:40 +0300

    logic gate

--- install.sh
+++ install.sh
@@ -36,9 +36,9 @@ main() {
   read -p "Would you also like to build the ollama model required for the reading? (Y/N) " ollamaPrompt
   if [[ "$ollamaPrompt" = "Y" || "$ollamaPrompt" = "y" ]]; then 
     ollama create tarotui -f src/utils/Ollama_custom/Modelfile
-    read -p "After creating the model: tarotui, would you like to delete the base model 'llama3.2', this would not affect anything in regards to the program (Y/N) " clean
+    read -p "After creating the model: tarotui, would you like to delete the base model 'deepseek-r1', this would not affect anything in regards to the program (Y/N) " clean
     if [[ "$clean" = "Y" || "$clean" = "y" ]]; then
-      ollama rm llama3.2
+      ollama rm deepseek-r1
     else 
       continue
     fi
