01d9bae2f3ceaf378c67b09b75f5ee4b313ccd2c
Bart <126595616+Tsukeruu@users.noreply.github.com>
2026-05-15 10:41:12 +0300

    Fix message initialization in custom error classes

--- src/utils/custom_errors.py
+++ src/utils/custom_errors.py
@@ -4,5 +4,11 @@ from requests.exceptions import RequestException
 class API_ERROR(Exception):
     def __init__(self, message: Union[str, RequestException] = None, error: RequestException = None) -> None:
         if not message:
-            message = f"AN UNKNOWN REQUEST ERROR HAS OCCURED! {error}"
+            message: str = f"AN UNKNOWN REQUEST ERROR HAS OCCURED! {error}"
+        super().__init__(message)
+
+class UNDETECTABLE_OS(Exception):
+    def __init__(self, command: str, message: str = None, error: Optional[str] = None) -> None:
+        if not message:
+            message: str = f"OS TO RUN COMMAND: `{command}` IS UNDETECTABLE"
         super().__init__(message)
